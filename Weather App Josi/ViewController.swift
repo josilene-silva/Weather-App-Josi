598501307d04d6f6487722f3216511c4bb2ccd74
Josilene Silva <josilene.silva.ext@bayer.com>
2023-11-20 11:59:52 -0400

    adiciona CustomView na ViewController

--- Weather App Josi/ViewController.swift
+++ Weather App Josi/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    //private let customView = UIView(frame: .zero)
+    
+    // executado quando a View é carregada (uma vez)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
     }
-
+    
+    private func setupView() {
+        // colore a View principal
+        view.backgroundColor = UIColor.red
+                       
+        setHierarchy()
+        setConstraitns()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraitns() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo:
+                view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
 
 }
 
